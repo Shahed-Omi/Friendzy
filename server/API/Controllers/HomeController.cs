ba74a0c077c9d4af9d20405af65999f4a0fabaa1
shahed.omi <shahed.omi@datasoft-bd.com>
2025-03-12 15:27:09 +0600

    fixed folder structure

---
+++ server/API/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class HomeController : BaseController
+    {
+        [HttpGet("GetHi")]
+        public IActionResult Get()
+        {
+            return Ok("Hi");
+        }
+    }
+}
